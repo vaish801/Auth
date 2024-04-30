5a93491a33873263b8c4f494f72d67a4f489134b
Vaishnavi Sawant <vvaishsawant@gmail.com>
2024-04-30 12:12:46 -0600

    create signup api routes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ console.log('Connected');
 
 const app=express();
 
+app.use(express.json());
+
 app.listen(3000,()=>{
     console.log('Server listening!!');
 });
@@ -23,3 +26,4 @@ app.listen(3000,()=>{
 //Path api/index.js
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
