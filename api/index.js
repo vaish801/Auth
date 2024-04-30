35b939df15f033f4197a81924c63e3668895a2b1
Vaishnavi Sawant <vvaishsawant@gmail.com>
2024-04-30 10:50:50 -0600

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then (()=>{
+console.log('Connected');
+})
+.catch((err)=>{
+    console.log(err);
+});
 
 const app=express();
+
 app.listen(300,()=>{
     console.log('Server listening!!');
 });
