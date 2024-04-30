2e11f4bccfdc98ae7019ea81064c2cf32d4a099a
Vaishnavi Sawant <vvaishsawant@gmail.com>
2024-04-30 12:25:36 -0600

    add a middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import User from "../models/user.model.js";
 
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const signup =async (req,res)=>{
+export const signup =async (req,res, next)=>{
     const {username,email,password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password,10);
     const newUser = new User({username, email, password : hashedPassword});
@@ -12,6 +13,7 @@ export const signup =async (req,res)=>{
         res.status(201).json({message:"User Created Successfully"});
     }
     catch(error){
-        res.status(500).json(error.message);
+        // next(errorHandler(300, "something went wrong"));
+        next(error);
     }
 };
