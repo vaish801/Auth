c9162878d6a2045617090dd24364ee0404052032
Vaishnavi Sawant <vvaishsawant@gmail.com>
2024-04-30 09:48:47 -0600

    add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">App</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/about' element={<About/>}></Route>
+      <Route path='sign-in' element={<SignIn/>}></Route>
+      <Route path='/sign-up' element={<SignUp/>}></Route>
+      <Route path='/profile' element={<Profile/>}></Route>
+    </Routes>
+    {/* <h1 className="text-3xl font-bold underline">App</h1> */}
+    </BrowserRouter>
   )
 }
 
